e9448818428063dfdd682ccf5caf4705afae6457
Sumit Chandra <chandra78.sumit@gmail.com>
2025-08-05 23:45:56 +0530

    me issue

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const { register, login } = require("../controllers/authController");
+const verifyToken = require("../middleware/authMiddleware")
 
 // POST /api/auth/register
 router.post("/register", register);
 
 // POST /api/auth/login
 router.post("/login", login);
+
+router.get("/me", verifyToken, (req, res) => {
+  res.json(req.user); // Sends back the decoded JWT (e.g., userId, iat, exp)
+});
 
 module.exports = router;
