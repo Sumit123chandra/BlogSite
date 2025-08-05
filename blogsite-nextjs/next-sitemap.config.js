e32eb5eb78884b826594e6834d7c48008d7b7559
Sumit Chandra <chandra78.sumit@gmail.com>
2025-08-06 04:16:29 +0530

    sitemap

--- blogsite-nextjs/next-sitemap.config.js
+++ blogsite-nextjs/next-sitemap.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://blog-site-silk-nine.vercel.app', // your live site URL
-  generateRobotsTxt: true, // generates robots.txt
-  sitemapSize: 7000,
+  siteUrl: 'https://blog-site-silk-nine.vercel.app',
+  generateRobotsTxt: true,
+  changefreq: 'daily',
+  priority: 0.7,
+  exclude: ['/admin/*'], // if any
+  additionalPaths: async (config) => {
+    return [
+      await config.transform(config, '/contact'),
+      await config.transform(config, '/privacy'),
+      await config.transform(config, '/about'),
+    ];
+  },
 };
